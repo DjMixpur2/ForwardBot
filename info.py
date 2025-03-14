a330de74851aaa542c7e18271f29fd5793537b07
DjMixpur2 <djmixpur2@gmail.com>
2025-03-14 14:12:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name}</code>")
-OWNER = environ.get("OWNER", "")
+API_ID = int(environ.get("API_ID", "21344245"))
+API_HASH = environ.get("API_HASH", "e72de40c666f1664f847a79f97dd3882")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7980904477:AAHQOHBwPES7dkDrgHPF40_leghWYQbIoFo")
+FILE_CAPTION = environ.get("FILE_CAPTION", "<code>{file_name} {caption}</code>")
+OWNER = environ.get("OWNER", "7585839477")
 PRIVATE_BOT = environ.get("PRIVATE_BOT", False)
 UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "https://t.me/webverseupdates")
 SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "https://t.me/webversegroup")
